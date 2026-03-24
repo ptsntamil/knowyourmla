d82ae0e5292c941ed488e152d259a4613003929a
Tamilarasan Pannerselvam <ptsntamil@gmail.com>
2026-03-24 22:41:27 +0530

    Enable speed insights

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -61,6 +62,7 @@ export default function RootLayout({
         </div>
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
